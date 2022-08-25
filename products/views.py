d2f2841e47788af22cde18fc56b9a6cb5a4272e2
Carson 723 <23cadememy@gmail.com>
2022-08-25 11:45:23 -0500

    Started api view file with products_list GET request

--- products/views.py
+++ products/views.py
@@ -1,7 +1,16 @@
+from itertools import product
+from urllib.request import Request
 from rest_framework.decorators import api_view
 from rest_framework.decorators import Response
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
+from products import serializers
+
 
 @api_view(['GET'])
 def products_list(request):
-
-    return Response('ok')
+     if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
