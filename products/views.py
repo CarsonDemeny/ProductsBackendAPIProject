c68ade1b87fe29b62d06948014334ab09fb8e0c4
Carson 723 <23cadememy@gmail.com>
2022-08-25 12:56:45 -0500

    Finished api view products list GET and POST

--- products/views.py
+++ products/views.py
@@ -3,14 +3,21 @@ from urllib.request import Request
 from rest_framework.decorators import api_view
 from rest_framework.decorators import Response
 from rest_framework import status
+
+import products
 from .serializers import ProductSerializer
 from .models import Product
 from products import serializers
 
 
-@api_view(['GET'])
-def products_list(request):
-     if request.method == 'GET':
+@api_view(['GET', 'POST'])
+def cars_list(request):
+    if request.method == 'GET':
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
